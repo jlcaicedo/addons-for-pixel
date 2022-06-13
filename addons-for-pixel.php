54e89734bcafb7d33c85826dd2cece07d36272a0
Jose Caicedo <jlcaicedo@outlook.com>
2022-06-13 10:39:03 -0500

    Actualizacion de la version estable

--- addons-for-pixel.php
+++ addons-for-pixel.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: Addons for Pixel 
+ * Plugin Name: Addons for Pixel
  * Description: Add variables for Facebook Pixel in wordpress
  * Plugin URI: https://sjsocialmedia.com/?utm_source=wp-plugins&utm_campaign=plugin-uri&utm_medium=wp-dash
  * Author: SJ Social Media
- * Version: 1.1
+ * Version: 1.2.2
  * Author URI: https://sjsocialmedia.com/?utm_source=wp-plugins&utm_campaign=author-uri&utm_medium=wp-dash
  *
  * Addons for Pixel is free software: you can redistribute it and/or modify
@@ -16,56 +16,56 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
 */
 
-// AFP_FPViewContent 
+// AFP_FPViewContent
 function AFP_FPViewContent() {
     ?>
-		<script> 
-		document.addEventListener("DOMContentLoaded", function(event) { 
+		<script>
+		document.addEventListener("DOMContentLoaded", function(event) {
 				fbq('track', 'ViewContent', {
 				  content_name: '<?php the_title(); ?>',
-				  content_type: '<?php $post_type = get_post_type( $post->ID ); echo $post_type; ?>',				   
+				  content_type: '<?php $post_type = get_post_type( $post->ID ); echo $post_type; ?>',
 				  content_ids: '<?php the_ID(); ?>',
-				}); 
-			}); 
+				});
+			});
 		</script>
     <?php
 }
 add_action('page_template', 'AFP_FPViewContent');
 
-// PixelViewPost 
+// PixelViewPost
 function AFP_FPViewPost() {
-    ?>		
-		<script> 
-		document.addEventListener("DOMContentLoaded", function(event) { 
+    ?>
+		<script>
+		document.addEventListener("DOMContentLoaded", function(event) {
 				fbq('track', 'ViewContent', {
 				  content_name: '<?php the_title(); ?>',
 				  content_category: '<?php $cat = get_the_category(); echo $cat[0]->cat_name; ?>',
 				  content_ids: '<?php the_ID(); ?>',
-				  content_type: '<?php $post_type = get_post_type( $post->ID ); echo $post_type; ?>',				   
-				}); 
-			}); 
-		</script>		
+				  content_type: '<?php $post_type = get_post_type( $post->ID ); echo $post_type; ?>',
+				});
+			});
+		</script>
     <?php
 }
 add_action('single_template', 'AFP_FPViewPost');
 
-// PixelProducts 
+// PixelProducts
 function AFP_FPProducts() {
     ?>
-		<script> 
-		document.addEventListener("DOMContentLoaded", function(event) { 
+		<script>
+		document.addEventListener("DOMContentLoaded", function(event) {
 				fbq('track', 'ViewContent', {
 				  content_name: '<?php the_title(); ?>',
 				  content_category: '<?php $cat = get_the_category(); echo $cat[0]->cat_name; ?>',
 				  content_ids: '<?php the_ID(); ?>',
 				  content_type: '<?php $post_type = get_post_type( $post->ID ); echo $post_type; ?>',
 				  currency: '<?php echo get_woocommerce_currency(); ?>',
-				  value: '<?php $product = wc_get_product(); echo $product->get_price(); ?>',				   
-				}); 
-			}); 
+				  value: '<?php $product = wc_get_product(); echo $product->get_price(); ?>',
+				});
+			});
 		</script>
     <?php
 }
@@ -77,7 +77,7 @@ function AFP_OpengraphDoctype( $output ) {
     }
 add_filter('language_attributes', 'AFP_OpengraphDoctype');
 
-//Lets add Open Graph Meta Info 
+//Lets add Open Graph Meta Info
 function AFP_FOpengraph() {
     global $post;
     if ( !is_singular()) //if it is not a post or a page
